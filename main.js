2082e340bc587c83f77696895ca1a679b7e2f802
anajullia <anajullia@anajullia.com>
2023-06-18 09:26:33 -0300

    Versão 0.0.1

--- main.js
+++ main.js
@@ -1,46 +1,17 @@
-// Código do movimento
-
 $(document).ready(function() {
-    var velocidade = 2;
-    
     $("#btum").mousedown(function() {
-      var interval = setInterval(function() {
-        $("#playerimg").css("top", "-=" + velocidade + "px");
-      }, 10);
-      
-      $(this).data('interval', interval);
-    }).mouseup(function() {
-      clearInterval($(this).data('interval'));
+      $("#playerimg").animate({ top: "-=50px" }, 500);
     });
-    
-    $("#btdois").mousedown(function() {
-      var interval = setInterval(function() {
-        $("#playerimg").css("top", "+=" + velocidade + "px");
-      }, 10);
-      
-      $(this).data('interval', interval);
-    }).mouseup(function() {
-      clearInterval($(this).data('interval'));
-    });
-    
-    $("#bttres").mousedown(function() {
-      var interval = setInterval(function() {
-        $("#playerimg").css("left", "-=" + velocidade + "px");
-      }, 10);
-      
-      $(this).data('interval', interval);
-    }).mouseup(function() {
-      clearInterval($(this).data('interval'));
-    });
-    
-    $("#btquatro").mousedown(function() {
-      var interval = setInterval(function() {
-        $("#playerimg").css("left", "+=" + velocidade + "px");
-      }, 10);
-      
-      $(this).data('interval', interval);
-    }).mouseup(function() {
-      clearInterval($(this).data('interval'));
-    });
+
+  $("#btdois").mousedown(function() {
+    $("#playerimg").animate({ top: "+=50px" }, 500);
   });
-  
+
+  $("#bttres").mousedown(function() {
+    $("#playerimg").animate({ left: "-=50px" }, 500);
+  });
+
+  $("#btquatro").mousedown(function() {
+    $("#playerimg").animate({ left: "+=50px" }, 500);
+  });
+})
